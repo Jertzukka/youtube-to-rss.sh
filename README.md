06b4e0cebe50c2ffb8abb387d029b0ce2e89da77
Jertzukka <jertzukka@gmail.com>
2020-11-11 20:23:13 +0200

    Fixes and made it POSIX compliant

--- README.md
+++ README.md
@@ -15,18 +15,21 @@ Newsboat itself has an import function which does serve a basic purpose of
 importing the OPML file exported from Youtube subscription manager and adds
 them to your 'urls' file. The way this script improves on top of this, is that 
 it allows you to easily add and delete subscriptions without having to manually 
-export them from the Youtube's website.
+export them from the Youtube's website. Made into POSIX compliant from the
+previous bash version of the script.
 
 Usage
 -----
 + Download the script for example by running `git clone https://github.com/Jertzukka/youtube-to-rss.sh`
 + Get your Channel ID by navigating Youtube to Settings and Advanced Settings. 
-Open the script in an editor, and replace the existing 'channelid=' with your own, 
-and save the file.
+You can edit the Channel ID straight into the script into `channelid=''` or 
+enter it when the script is ran first time and it's asked.
++ You need to get a Youtube Data API v3 key and insert it into `apikey=''`,
+first I tried to provide mine for public use, but they're abused.
 + If necessary, make the file executable by running `chmod +x ./youtube-to-rss.sh`
 + Run the script with `./youtube-to-rss.sh`
 
 Dependencies
 ------------
-+ **bash**
++ **sh**
 + **jq**, lightweight and flexible command-line JSON processor.
--- youtube-to-rss.sh
+++ youtube-to-rss.sh
@@ -1,19 +1,27 @@
-#!/bin/bash
+#!/bin/sh
+
 
 # Config
-apikey="AIzaSyCdJeEu_WsIn-ckh2QGX5hnJlPSivRlA0Q"
-channelid="UCaBX7ogjBF_oeYo20zO_Y9h"
+channelid=''
+apikey='AIzaSyCdJeEu_WsIn-ckh2QGX5hnJlPSivRlA0Q'
 urls="$HOME/.newsboat/urls"
 youtuberss="https://www.youtube.com/feeds/videos.xml?channel_id="
 
-if [ ! -f $urls ]; then
+
+# Check if urls file is missing..
+if [ ! -f "$urls" ]; then
     echo "$urls is not found. Please create the file first or give the correct location."
     exit
 fi
 
-if [[ $channelid = "UCaBX7ogjBF_oeYo20zO_Y9h" ]]; then
-    echo "Change the channelid on the script to yours to retrieve your subscriptions."
-    exit
+
+# First time setup to set channel ID.
+if [ -z $channelid ]; then
+    echo "First time setup. The default API key will not work, you need to edit it with a working one. Also edit your channel ID into the script or add it now:"
+    read -r newid
+    editrow=$(grep -n -m 1 "channelid=''" "$0" | cut -d : -f 1)
+    sed -i "${editrow}s/channelid=''/channelid='$newid'/" $0
+    channelid=$newid
 fi
 
 
@@ -25,7 +33,7 @@ fi
 
 
 # Cleanup of temp files.
-function cleanup () {
+cleanup () {
     if [ -f lastreq.json ]; then rm lastreq.json; fi
     if [ -f titles.json ]; then rm titles.json; fi
     if [ -f channelids.json ]; then rm channelids.json; fi
@@ -43,7 +51,7 @@ trap '
 # Request all subscriptions from Youtube API and write channelids and titles into files.
 while true; do
     # Change request based on whether we have a newPageToken yet
-    if [ ! -z "$nextPageToken" ]; then
+    if [ -n "$nextPageToken" ]; then
         curl -s "https://www.googleapis.com/youtube/v3/subscriptions?part=snippet&channelId=$channelid&maxResults=50&pageToken=$nextPageToken&key=$apikey" \
         --header "Accept: application/json" --compressed | jq . > lastreq.json
     else
@@ -52,7 +60,7 @@ while true; do
     fi
 
     # Catch errors
-    if [[ $(jq .error.code lastreq.json) != "null" ]]; then
+    if [ "$(jq .error.code lastreq.json)" != "null" ]; then
         echo "Code: $(jq .error.code lastreq.json), Error: $(jq .error.errors[].message lastreq.json)"
         cleanup
         exit
@@ -60,12 +68,12 @@ while true; do
 
     retrieved=$(jq '.items | length' lastreq.json)
     nextPageToken=$(jq -r .nextPageToken lastreq.json)
-    total=$(( $total + $retrieved ))
+    total=$(( total + retrieved ))
     echo "Retrieved $total out of $(jq .pageInfo.totalResults lastreq.json)."
     jq -r .items[].snippet.title lastreq.json >> titles.json
     jq -r .items[].snippet.resourceId.channelId lastreq.json >> channelids.json
     # Break out if next page doesn't exist
-    if [[ $nextPageToken = "null" ]]; then
+    if [ "$nextPageToken" = "null" ]; then
         echo "Successfully retrieved $total channels out of $(jq .pageInfo.totalResults lastreq.json)."
         break;
     fi
@@ -74,44 +82,59 @@ done
 
 # Add new channels which aren't on urls yet.
 printf "\nAdd tags to imported Youtube channels? (or leave  empty)   "
-read tag
-linecount=$(wc -l <channelids.json)
-for i in $( seq 1 $linecount ); do
+read -r tag
+linecount=$(wc -l < channelids.json)
+updated=false
+for i in $( seq 1 "$linecount" ); do
     channelid=$(sed "${i}q;d" channelids.json)
     title=$(sed "${i}q;d" titles.json)
-    if ! grep -q $channelid $urls; then
-        echo "Adding $title."
-        if [[ $tag != "" ]]; then
-            echo "${youtuberss}$(sed "${i}q;d" channelids.json) \"~$title\" \"$tag\" #Automated" >> $urls
+    if ! grep -q "$channelid" "$urls"; then
+        echo "Adding $title ($channelid)"
+        updated=true
+        if [ "$tag" != "" ]; then
+            echo "${youtuberss}$(sed "${i}q;d" channelids.json) \"~$title\" \"$tag\" #Automated" >> "$urls"
         else
-            echo "${youtuberss}$(sed "${i}q;d" channelids.json) \"~$title\" #Automated" >> $urls
+            echo "${youtuberss}$(sed "${i}q;d" channelids.json) \"~$title\" #Automated" >> "$urls"
         fi
     fi
 done
-echo -e "\nAll new subscriptions added!"
+if [ "$updated" = true ]; then
+    printf "All new subscriptions added!\n"
+else
+    printf "Nothing to do.\n"
+fi
 
 
 # Delete channels that have been added with this tool, but you no longer subscribe to.
 printf "\nDelete channels which have been added with this tool, but you no longer subscribe to? (y/n)   "
-read delete
-linecount=$(wc -l <$urls)
-if [[ $delete = "y" ]] || [[ $delete = "Y" ]]; then
-    for (( i=$linecount; i>=1; i-- )); do
-        line=$(sed "${i}q;d" $urls)
-        if [[ $line == *"#Automated"* ]]; then
-            channelid=$(echo $line | sed 's/ .*//' | cut -d= -f 2)
-            title=$(sed "${i}q;d" titles.json)
-            if ! grep -q $channelid $urls; then
-                echo "$title has been unsubscribed, deleting."
-                removeline=$(grep -n $channelid $urls | cut -d : -f 1)
-                sed -e "${removeline}d" $urls
+read -r delete
+linecount=$(wc -l < "$urls")
+updated=false
+if [ "$delete" = "y" ] || [ "$delete" = "Y" ]; then
+    : $((i=linecount))
+    while [ "$((i>=1))" -ne 0 ]; do
+        line=$(sed "${i}q;d" "$urls")
+        if test "${line#*'#Automated'}" != "$line"; then
+            channelid=$(echo "$line" | sed 's/ .*//' | cut -d= -f 2)
+            title=$(sed "${i}q;d" "$urls" | sed 's/^[^"]*"\([^"]*\)".*/\1/' | sed 's/~//')
+            if ! grep -q "$channelid" channelids.json; then
+                echo "$title ($channelid) has been unsubscribed, deleting."
+                updated=true
+                removeline=$(grep -n "$channelid" "$urls" | cut -d : -f 1)
+                sed -i "${removeline}d" "$urls"
             fi
         fi
+        : $((i -= 1))
     done
-    echo -e "\nAll subscriptions up-to date!"
+    if [ "$updated" = true ]; then
+        printf "All subscriptions up-to date!\n"
+    else
+        printf "Nothing to do.\n"
+    fi
 else
     echo "Exiting."
 fi
+
 
 cleanup
 exit
